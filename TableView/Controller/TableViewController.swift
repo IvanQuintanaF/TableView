a9c822acf0a5f176328c9e4e9b692245a28eceb9
ivanquintanaf <quintana9506@gmail.com>
2019-03-25 19:15:13 -0600

    data added

--- TableView/Controller/TableViewController.swift
+++ TableView/Controller/TableViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    let cellId = "cellId"
+    
+    let data = [
+        "uno",
+        "dos",
+        "tres",
+        "cuatro",
+        "cinco"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        view.backgroundColor = .white
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 }
 
