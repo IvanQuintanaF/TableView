fb885c590ae62a3745c6c47b07fedecd8494e40e
ivanquintanaf <quintana9506@gmail.com>
2019-03-25 18:57:21 -0600

    navigation controller and tableview controller added

--- TableView/Base/AppDelegate.swift
+++ TableView/Base/AppDelegate.swift
@@ -16,9 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .red
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: TableViewController())
+        window?.rootViewController = navigationController
         return true
     }
 
